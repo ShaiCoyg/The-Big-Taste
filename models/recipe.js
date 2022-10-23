0c5f0b5050d5617144066a4cde1f9eec9e9a29d2
Shai <55280978+ShaiCoyg@users.noreply.github.com>
2022-10-23 12:19:24 +0300

    style

--- models/recipe.js
+++ models/recipe.js
@@ -1,49 +1,51 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const recipesSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    tags: {
-        type: String,
-        required: true
-    },
-    prepTime: {
-        type: Number,
-        required: true
-    },
-    cookTime: {
-        type: Number,
-        required: true
-    },
-    ingredients: {
-        type: String,
-        required: true
-    },
-    directions: {
-        type: String,
-        required: true
-    },
-    posterImage: {
-        type: Buffer,
-        required: true
-    },
-    posterImageType: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: String,
+    required: true,
+  },
+  prepTime: {
+    type: String,
+    required: true,
+  },
+  cookTime: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: String,
+    required: true,
+  },
+  directions: {
+    type: String,
+    required: true,
+  },
+  posterImage: {
+    type: Buffer,
+    required: true,
+  },
+  posterImageType: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
 
-recipesSchema.virtual('posterImagePath').get(function () {
-    if (this.posterImage != null && this.posterImageType != null) {
-        return `data:${this.posterImageType};charset=utf-8;base64,${this.posterImage.toString('base64')}`
-    }
-})
+recipesSchema.virtual("posterImagePath").get(function () {
+  if (this.posterImage != null && this.posterImageType != null) {
+    return `data:${
+      this.posterImageType
+    };charset=utf-8;base64,${this.posterImage.toString("base64")}`;
+  }
+});
 
-module.exports = mongoose.model('Recipe', recipesSchema)
+module.exports = mongoose.model("Recipe", recipesSchema);
